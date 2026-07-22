5cf6ef77c8d7ce33490be73457e5d22afe05243a
Mounbagna Abdella Abasse <abdellaabasse@iut-dhaka.edu>
2026-07-22 16:36:17 +0100

    update db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,11 +6,15 @@ dotenv.config();
 const { Pool } = pg;
 
 const db = new Pool({
-  host: process.env.DB_HOST,
+  /*host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.Db_PORT
+  port: process.env.Db_PORT*/
+  connectionString: process.env.DATABASE_URL,
+  ssl:{
+    rejectUnauthorized:false
+  }
 });
 
 db.connect((err) => {
