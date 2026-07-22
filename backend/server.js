749746798b42d60c4d970e3be17ab0e3b1593051
Mounbagna Abdella Abasse <abdellaabasse@iut-dhaka.edu>
2026-07-22 18:21:47 +0100

    fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,59 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
-
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/authRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
-import shopOwnerRoutes from "./routes/shopOwnerRoutes.js"
-import adminRoutes from "./routes/adminRoutes.js"
-import categoryRoutes from "./routes/categoryRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import shopOwnerRoutes from "./routes/shopOwnerRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: [
-  "https://easymarket-13yu1rxj7-mounbagnas-projects.vercel.app",
-  "easymarket-git-main-mounbagnas-projects.vercel.app",
-  "easymarket-tau.vercel.app"], 
-  methods: ["GET", "POST", "PUT", "DELETE"], credentials: true})); //CORS FIX
-app.use(express.json({limit: "50mb"})); //BODY PARSER
-app.use(express.urlencoded({extended:true,limit: "50mb"})) //Form requests (optional)
-app.use("/uploads", express.static("uploads")); //STATIC FILE ACCESS
 
-if (!fs.existsSync("uploads")) {fs.mkdirSync("uploads");} //CREATE UPLOADS FOLDER IF NOT EXISTS
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://easymarket-tau.vercel.app"
+];
 
-/* ---------------- ROUTES ---------------- */
-app.use("/",authRoutes);
-app.use("/products",productRoutes);
-app.use("/shop",shopOwnerRoutes);
-app.use("/admin",adminRoutes);
-app.use("/categories",categoryRoutes);
 
-/* ---------------- START SERVER ---------------- */
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({
+  extended:true,
+  limit:"50mb"
+}));
+
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
+
+app.use("/uploads", express.static("uploads"));
+
+
+app.use("/", authRoutes);
+app.use("/products", productRoutes);
+app.use("/shop", shopOwnerRoutes);
+app.use("/admin", adminRoutes);
+app.use("/categories", categoryRoutes);
+
+
 app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+  console.log("Server running on port 5000");
 });
