cc7586c229de5280b4355d4680034743c20e684b
Mounbagna Abdella Abasse <abdellaabasse@iut-dhaka.edu>
2026-07-22 17:17:16 +0100

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:3000", methods: ["GET", "POST", "PUT", "DELETE"], credentials: true})); //CORS FIX
+app.use(cors({origin: ["http://localhost:3000",
+  "https://easymarket-13yu1rxj7-mounbagnas-projects.vercel.app/"], 
+  methods: ["GET", "POST", "PUT", "DELETE"], credentials: true})); //CORS FIX
 app.use(express.json({limit: "50mb"})); //BODY PARSER
 app.use(express.urlencoded({extended:true,limit: "50mb"})) //Form requests (optional)
 app.use("/uploads", express.static("uploads")); //STATIC FILE ACCESS
